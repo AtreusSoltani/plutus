fab7ff26a7b7d6e7bbdb33dfa53a34a586892187
Abolfazl Soltani <abolfazl.soltani@sotoon.ir>
2023-07-16 12:17:15 +0800

    Add docker compose

--- plutus/settings.py
+++ plutus/settings.py
@@ -85,9 +85,20 @@ WSGI_APPLICATION = "plutus.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'plutus',
+
+        'USER': 'plutus',
+
+        'PASSWORD': 'plutus',
+
+        'HOST': 'postgres',
+
+        'PORT': '5432',
+
     }
 }
 
